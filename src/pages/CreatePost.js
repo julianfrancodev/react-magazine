5ba3ca91b5420fcc99c5736d2886d23e0babeb46
JULIAN <juliandanielfranco@gmail.com>
2020-08-20 21:32:19 -0500

    feat: add new plugin for production

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -4,19 +4,23 @@ import { DraftailEditor } from "draftail";
 import { EditorState } from "draft-js";
 import createInlineToolbarPlugin from "draft-js-inline-toolbar-plugin";
 import createSideToolbarPlugin from "draft-js-side-toolbar-plugin";
+import createHashtagPlugin from 'draft-js-hashtag-plugin';
 
 import "draft-js/dist/Draft.css";
 import "draftail/dist/draftail.css";
 import "draft-js-inline-toolbar-plugin/lib/plugin.css";
 import "draft-js-side-toolbar-plugin/lib/plugin.css";
+import 'draft-js-hashtag-plugin/lib/plugin.css';
 
+
+const hashtagPlugin = createHashtagPlugin();
 
 const inlineToolbarPlugin = createInlineToolbarPlugin();
 const { InlineToolbar } = inlineToolbarPlugin;
 const sideToolbarPlugin = createSideToolbarPlugin();
 const { SideToolbar } = sideToolbarPlugin;
 
-const plugins = [inlineToolbarPlugin, sideToolbarPlugin];
+const plugins = [inlineToolbarPlugin, sideToolbarPlugin, hashtagPlugin];
 export default function CreatePost() {
 
   const editor = useRef(null);
